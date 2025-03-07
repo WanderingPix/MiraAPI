b16b41df5b2f478f915b32e7a922913200257d9f
WanderingPixel <126269533+WanderingPix@users.noreply.github.com>
2025-03-07 19:23:17 +0100

    Create Flash.cs (for example Mod)

---
+++ MiraAPI.Example/Modifiers/Flash.cs
@@ -0,0 +1,22 @@
+﻿using MiraAPI.Example.Options.Modifiers;
+using MiraAPI.GameOptions;
+using MiraAPI.Modifiers.Types;
+using UnityEngine;
+
+namespace MiraAPI.Example.Modifiers;
+
+public class FlashModifier : GameModifier
+{
+    public override string ModifierName => "Flash";
+
+    public override int GetAssignmentChance()
+    {
+        return (int)OptionGroupSingleton<CaptainModifierSettings>.Instance.Chance;
+    }
+
+    public override int GetAmountPerGame()
+    {
+        return (int)OptionGroupSingleton<CaptainModifierSettings>.Instance.Amount;
+    }
+    public override Vector2 Speed => new Vector2(3f, 3f);
+}
